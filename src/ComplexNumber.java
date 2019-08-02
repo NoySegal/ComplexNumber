91fa52cfd51d9f7f7b3a8079dbd91523c0e61deb
NoySegal <segalmaterial@gmail.com>
2019-08-02 19:12:48 +0300

    add complex product and divide

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,17 +1,39 @@
 public class ComplexNumber {
 
-    int realPart;
-    int imaginaryPart;
+    private double re;
+    private double im;
 
-    public ComplexNumber(int real, int imaginary) {
-        realPart = real;
-        imaginaryPart = imaginary;
+    public ComplexNumber(double realPart, double imaginaryPart) {
+        this.re = realPart;
+        this.im = imaginaryPart;
     }
 
-    public ComplexNumber Plus(ComplexNumber other) {
-        ComplexNumber result = new ComplexNumber();
-        result.realPart = realPart + other.realPart;
-        result.imaginaryPart = imaginaryPart + other.imaginaryPart;
+    public ComplexNumber plus(ComplexNumber other) {
+        double newRe = this.re + other.re;
+        double newIm = this.im + other.im;
+        ComplexNumber result = new ComplexNumber(newRe, newIm);
+        return result;
+    }
+
+    public ComplexNumber minus(ComplexNumber other) {
+        double newRe = this.re - other.re;
+        double newIm = this.im - other.im;
+        ComplexNumber result = new ComplexNumber(newRe, newIm);
+        return result;
+    }
+
+    public ComplexNumber times(ComplexNumber other) {
+        double newRe = this.re * other.re - this.im * other.im;
+        double newIm = this.re * other.im + this.im * other.re;
+        ComplexNumber result = new ComplexNumber(newRe, newIm);
+        return result;
+    }
+
+    public ComplexNumber divide(ComplexNumber other) {
+        double denominator = other.re * other.re + other.im * other.im;
+        double newRe = (this.re * other.re + this.im * other.im) / denominator;
+        double newIm = (this.im * other.re - this.re * other.im) / denominator;
+        ComplexNumber result = new ComplexNumber(newRe, newIm);
         return result;
     }
 
